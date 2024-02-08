52123de13de9dec521710b4376f66e4da2d7a2a8
catisf <cati.sanches@gmail.com>
2024-02-08 17:43:25 +0000

    finished map and added html banners with questions

--- barchart_dependencies/static/js/map_logic.js
+++ barchart_dependencies/static/js/map_logic.js
@@ -6,34 +6,53 @@ anychart.onDocumentReady(function () {
     anychart.data.loadJsonFile("/print_sample_data_all", function (data) {
         mapData = JSON.parse(data.cleaned_data_all);
 
-        console.log(mapData)
+        //console.log(mapData)
 
         // Define an empty array to store the job counts for each country
 
         d3.json("./static/js/countries.geojson").then(function(geojson) {
             let geoData = geojson.features;
-            console.log(geoData)
+            //console.log(geoData)
 
-            // Sum the number of jobs in each country
+            // Sum the number of jobs in each country and calculate sum salary
             let numJobs = {};
+            let salariesUSD = {};
+            let salariesGBP = {};
+
             mapData.forEach(function(job) {
                 let country = job.company_location; 
+                let USD = job.salary_in_usd; 
+                let GBP = job.salary_in_gbp; 
+
                 if (!numJobs[country]) {
                     numJobs[country] = 0;
+                    salariesUSD[country] = 0;
+                    salariesGBP[country] = 0;
                 }
                 numJobs[country]++;
+                salariesUSD[country] += USD;
+                salariesGBP[country] += GBP; 
             });
 
             // Save data in the right format for anychart
             let countryData = [];
+            let avgUSD = {};
+            let avgGBP = {};
+
             Object.keys(numJobs).forEach(function(country) {
+            
+            // calculate average salary in each country
+            avgUSD[country] = salariesUSD[country] / numJobs[country];
+            avgGBP[country] = salariesGBP[country] / numJobs[country];
+         
+            // Find the feature in geoData that matches the country name           
             let countryID = null;
-            // Find the feature in geoData that matches the country name
             let feature = geoData.find(feature => feature.properties.ADMIN === country); 
             if (feature) {
                 countryID = feature.properties.ISO_A2; 
             }
-            countryData.push({ id: countryID, value: numJobs[country], name: country });
+            countryData.push({ id: countryID, value: numJobs[country], name: country,
+            salaryUSD: avgUSD[country], salaryGBP: avgGBP[country] });
             });       
 
             // Connect the data with the map
@@ -47,8 +66,7 @@ anychart.onDocumentReady(function () {
 
             // Define color scale ranges
             var ocs = anychart.scales.ordinalColor([
-                { less: 10 },
-                { from: 10, to: 99 },
+                { less: 100 },
                 { from: 100, to: 199 },
                 { from: 200, to: 299 },
                 { from: 300, to: 399 },
@@ -58,7 +76,7 @@ anychart.onDocumentReady(function () {
             ]);
 
             // Set scale colors
-            ocs.colors(["rgb(236,231,242)","rgb(208,209,230)", "rgb(166,189,219)", "rgb(116,169,207)", "rgb(54,144,192)", "rgb(5,112,176)", "rgb(4,90,141)", "rgb(2,56,88)"]);       
+            ocs.colors(["rgb(208,209,230)", "rgb(166,189,219)", "rgb(116,169,207)", "rgb(54,144,192)", "rgb(5,112,176)", "rgb(4,90,141)", "rgb(2,56,88)"]);       
 
             // Apply color scale to the series
             series.colorScale(ocs);
@@ -69,10 +87,18 @@ anychart.onDocumentReady(function () {
             // Improve tooltip
             series.tooltip()
             .useHtml(true)
-            .format(function (d) {
-                return ("<h6 style='font-size:14px; font-weight:400; margin: 0.2rem 0;'> Jobs added:" + d.value + "</h6>");
+            .format(function (data) {
+                let country = data.name;
+                let countryInfo = countryData.find(countryInfo => countryInfo.name === country);
+                let salaryUSD = countryInfo.salaryUSD;
+                let salaryGBP = countryInfo.salaryGBP;
+
+                return ("<h5 style='font-size:14px; font-weight:400; margin: 0.2rem 0;'> Number of jobs: " + data.value + "</h5>Average salary (USD): $" +
+                salaryUSD.toLocaleString('en-US', { maximumFractionDigits: 0 }) + "<br>Average salary (GBP): £" + salaryGBP.toLocaleString('en-US', { maximumFractionDigits: 0 }));
             });
 
+            // Set map title
+            chart.title("Number of data-related jobs advertised between 2020-2023")
             // Set the container id
             chart.container('mapContainer');
 
